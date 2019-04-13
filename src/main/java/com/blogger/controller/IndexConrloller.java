d733dbf3258979c1067e94dc1cbb9370ffacf3b4
Luciana Estevamgit <luestevam@hotmail.com>
2019-04-13 09:07:10 -0400

    terminou configuracao servidor embarcado

---
+++ src/main/java/com/blogger/controller/IndexConrloller.java
@@ -0,0 +1,14 @@
+package com.blogger.controller;
+
+import java.io.Serializable;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+
+@Named
+@ViewScoped
+public class IndexConrloller implements Serializable {
+    
+    public IndexConrloller(){
+    }
+}
